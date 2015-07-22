eeb517c1fa30b3726da6d8c425ad2199e7a4e51d
Carl Lemaire <carl.lemaire@usherbrooke.ca>
2015-07-21 23:22:35 -0400

    On coupe un peu partout a cause de l'austerite: il faut s'y faire

--- src/DoublyConnectedEdgeList.h
+++ src/DoublyConnectedEdgeList.h
@@ -3,35 +3,23 @@
 #include <vector>
 #include <list>
 #include <memory>
-
-#include "BeachLine.h"
+#include "Point.h"
 
 using namespace std;
-
-/*
-LIMITES:
-	- Si un sommet est créé sur une arête, cette dernière n'est pas fractionnée en deux.
-	- Si deux arêtes se rejoignant en un sommet forment un angle trop petit, il peut y avoir de mauvais résultats.
-	- Si on ajoute une arête qui part ou arrive sur une arête existante, cette dernière n'est pas fractionnée.
-	- Lorsqu'une region est fractionnée en deux, deux nouvelles sont créées et l'ancienne est supprimée. Un pointeur sur l'ancienne devient invalide.
-	- On considère que le contour extérieur du graphe délimite une region.
-*/
 
 class Region;
 
 class HalfEdge
 {
 public:
-	Coord origin;
+	Point origin;
 	shared_ptr<Region> region;
 	shared_ptr<HalfEdge> twin;
 	shared_ptr<HalfEdge> next;
 	shared_ptr<HalfEdge> prev;
-	bool line_seen; // Used by get_lines()
 
-	HalfEdge(Coord a_origin)
+	HalfEdge(Point a_origin)
 	: origin(a_origin)
-	, line_seen(false)
 	{}
 	bool is_linked()
 	{
@@ -50,19 +38,15 @@ public:
 		}
 		return true; // We have looped around a region
 	}
-	Coord to_vector()
-	{
-		return Coord(twin->origin.x - origin.x, twin->origin.y - origin.y);
-	}
 };
 
 class Vertex
 {
 public:
-	Coord point;
+	Point point;
 	shared_ptr<HalfEdge> an_edge;
 
-	Vertex(Coord a_point)
+	Vertex(Point a_point)
 	{
 		point = a_point;
 	}
@@ -82,27 +66,16 @@ public:
 class DoublyConnectedEdgeList
 {
 public:
-	shared_ptr<HalfEdge> begin_edge(Coord point);
-	void complete_edge(shared_ptr<HalfEdge> half_edge_start, Coord point);
-	shared_ptr<HalfEdge> add_edge(const Coord start, const Coord end);
-	vector<pair<Coord, Coord>> get_lines();
-	void close_boundaries(double left, double bottom, double right, double top);
-	list<shared_ptr<HalfEdge>> get_half_edge_unliked();
+	shared_ptr<HalfEdge> begin_edge(Point point);
+	void complete_edge(shared_ptr<HalfEdge> half_edge_start, Point point);
+	shared_ptr<HalfEdge> add_edge(const Point start, const Point end);
 
-	DoublyConnectedEdgeList() : no_line_seen(true)
+	DoublyConnectedEdgeList()
 	{}
 
-private:
 	list<shared_ptr<Region>> regions;
 	vector<shared_ptr<HalfEdge>> edges;
 	vector<Vertex> vertices;
-	list<shared_ptr<HalfEdge>> unlinked_edges;
-	bool no_line_seen; // used by get_lines()
 
-	vector<shared_ptr<HalfEdge>> get_edges_touching_point(Coord point);
-	shared_ptr<HalfEdge> find_adjac_half_edge(bool is_next, shared_ptr<HalfEdge> half_edge);
-	shared_ptr<HalfEdge> find_next_half_edge(shared_ptr<HalfEdge> half_edge);
-	shared_ptr<HalfEdge> find_prev_half_edge(shared_ptr<HalfEdge> half_edge);
-	void connect_all_edges();
 	void assign_region_to_loop(shared_ptr<Region>, shared_ptr<HalfEdge>);
 };
