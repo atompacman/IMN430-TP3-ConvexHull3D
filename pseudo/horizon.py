71b886fc9b675bb33259c826c5a1a4d13ec707cf
Carl Lemaire <carl.lemaire@usherbrooke.ca>
2015-07-25 08:56:30 -0400

    Corrigé technique pour trouver l'horizon

--- pseudo/horizon.py
+++ pseudo/horizon.py
@@ -6,50 +6,41 @@ def getOrderedHorizonHalfEdges(point_node, D):
 
 	# Trouver un premier half-edge faisant partie de l'horizon
 	for face in point_node.conflits:
-		horizon_half_edge = getHalfEdgeTouchingHorizon(face)
+		horizon_half_edge = getHalfEdgeTouchingHorizon(face, point_node)
 		if horizon_half_edge is not None:
 			horizon.append(horizon_half_edge)
 			break
 
 	# Trouver les half-edge suivants sur l'horizon
-	# A partir d'une face, trouver une face adjacente (non-visitee) ayant un half-edge sur l'horizon
-	face = horizon[0].face
-	faces_visitees = []
-	while True:
-		faces_visitees.append(face)
-		# Boucler sur les half-edges de la face
-		premier_half_edge = face.an_edge
-		half_edge = premier_half_edge
-		continuer = True
-		trouve = False
-		while continuer:
-			adjac_face = half_edge.twin.face
-			if adjac_face not in faces_visitees:
-				# Verifier si la face adjacente contient un half-edge bordant l'horizon
-				horizon_half_edge = getHalfEdgeTouchingHorizon(adjac_face)
-				if horizon_half_edge is not None:
-					trouve = True
-					horizon.append(horizon_half_edge)
-					face = adjac_face
-					break
-			# Suite de la boucle
-			half_edge = half_edge.next
-			continuer = half_edge != premier_half_edge
-		if not trouve:
-			# On a termine, car on a visite toutes les faces sur l'horizon
-			break
+	# A partir d'un half-edge, verifier les half-edges touchant son extremite
+	half_edge = findNextHorizonHalfEdge(horizon[0], point_node)
+	while half_edge != horizon[0]:
+		horizon.append(half_edge)
+		half_edge.findNextHorizonHalfEdge(half_edge, point_node)
 
 	return horizon
 
-def getHalfEdgeTouchingHorizon(face):
+def findNextHorizonHalfEdge(half_edge_from, point_node):
+	half_edge = half_edge_from.next
+	continuer = True
+	while continuer:
+		# Verifier si le half-edge borde la region visible
+		if half_edge.twin.face not in point_node.conflits:
+			return half_edge
+		# Suite de la boucle
+		half_edge = half_edge.twin.next
+		continuer = half_edge != half_edge_from
+
+	return None # Erreur! Le half-edge fourni ne borde pas l'horizon!
+
+def getHalfEdgeTouchingHorizon(face, point_node):
 	# Boucler sur les half-edges de la face
 	premier_half_edge = face.an_edge
 	half_edge = premier_half_edge
 	continuer = True 
 	while continuer:
 		# Si twin fait partie d'une face invisible, on a trouve
-		adjac_face = half_edge.twin.face
-		if adjac_face not in point_node.conflits:
+		if half_edge.twin.face not in point_node.conflits:
 			return half_edge
 		# Suite de la boucle
 		half_edge = half_edge.next
